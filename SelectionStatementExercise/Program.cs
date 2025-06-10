56de8a4cdc2dbc8fc480f40b0a867726ae3fb2cd
tishacapps0519 <cappstisha@gmail.com>
2025-06-09 22:43:11 -0400

    exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber = random.Next(1,11);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Give me a number between 1 and 10:");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {response}");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine($"Too high!\nYour guess; {response}");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!\nThe favorite number was {favNumber}");  
+                }
+                
+            }
+
+           
+            ;
         }
     }
 }
